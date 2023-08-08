68b7b9c6e7f0c46933988ca3ef2d650b22c8ebdf
Saurabh Prakash <saurabhp75@gmail.com>
2023-08-09 02:02:30 +0530

    Fix transient login button on refresh

--- app/auth-button-client.tsx
+++ app/auth-button-client.tsx
@@ -1,23 +1,18 @@
 "use client";
 
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import {
+  type Session,
+  createClientComponentClient,
+} from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 
-function AuthButton() {
+export default function AuthButtonClient({
+  session,
+}: {
+  session: Session | null;
+}) {
   const supabase = createClientComponentClient();
   const router = useRouter();
-
-  const [session, setSession] = useState();
-
-  useEffect(() => {
-    const getSession = async () => {
-      const { data } = await supabase.auth.getSession();
-      setSession(data.session);
-    };
-
-    getSession();
-  });
 
   const handleSignIn = async () => {
     console.log("SignIn clicked");
@@ -41,5 +36,3 @@ function AuthButton() {
     <button onClick={handleSignIn}>Login</button>
   );
 }
-
-export default AuthButton;
