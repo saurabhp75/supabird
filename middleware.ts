2736bd1bbf299caa857a3c21db52f6309b10fe0d
Saurabh Prakash <saurabhp75@gmail.com>
2023-08-08 06:01:21 +0530

    Add cookie refresh logic in middleware

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+  // Refresh the session if its expired
+  await supabase.auth.getSession();
+  return res;
+}
