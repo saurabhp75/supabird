d528fde3650edd9be1294130d660b70070fed99d
Saurabh Prakash <saurabhp75@gmail.com>
2023-08-09 00:04:07 +0530

    Show Login/Logout button conditionally

--- app/auth-button.tsx
+++ app/auth-button.tsx
@@ -1,9 +1,23 @@
 "use client";
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 function AuthButton() {
   const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  const [session, setSession] = useState();
+
+  useEffect(() => {
+    const getSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      setSession(data.session);
+    };
+
+    getSession();
+  });
 
   const handleSignIn = async () => {
     console.log("SignIn clicked");
@@ -18,13 +32,13 @@ function AuthButton() {
   const handleSignOut = async () => {
     console.log("SignOut clicked");
     await supabase.auth.signOut();
+    router.refresh();
   };
 
-  return (
-    <>
-      <button onClick={handleSignIn}>Login</button>
-      <button onClick={handleSignOut}>Logout</button>
-    </>
+  return session ? (
+    <button onClick={handleSignOut}>Logout</button>
+  ) : (
+    <button onClick={handleSignIn}>Login</button>
   );
 }
 
