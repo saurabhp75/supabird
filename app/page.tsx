46d9621b36fe93afe0786c230cb1a3fcfbd1d4c4
Saurabh Prakash <saurabhp75@gmail.com>
2023-08-12 16:18:55 +0530

    Add likes to tweets

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { cookies } from "next/headers";
 import AuthButtonServer from "./auth-button-server";
 import { redirect } from "next/navigation";
 import NewTweet from "./new-tweet";
+import Likes from "./likes";
 
 export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies });
@@ -16,17 +17,34 @@ export default async function Home() {
     redirect("/login");
   }
 
-  const { data: tweets } = await supabase
+  const { data } = await supabase
     .from("tweets")
-    .select("*, profiles(*)");
+    .select("*, profiles(*), likes(*)");
   // We can grab colums frm related tables
   // from same schema, if RLS allows us
+
+  const tweets =
+    data?.map((tweet) => ({
+      ...tweet,
+      user_has_liked_tweet: !!tweet.likes.find(
+        (like) => like.user_id === session.user.id
+      ),
+      likes: tweet.likes.length,
+    })) ?? [];
 
   return (
     <div>
       <AuthButtonServer />
       <NewTweet />
-      <pre>{JSON.stringify(tweets, null, 2)}</pre>
+      {tweets?.map((tweet) => (
+        <div key={tweet.id}>
+          <p>
+            {tweet?.profiles?.name} {tweet?.profiles?.username}
+          </p>
+          <p>{tweet.title}</p>
+          <Likes tweet={tweet} />
+        </div>
+      ))}
     </div>
   );
 }
